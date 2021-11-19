a0b0dd6a1d87f581475d512c2d3cc47afa820fb6
yr <yr@qq.com>
2021-11-19 11:09:32 +0800

    first commit
    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xooooooox/gomysql
+
+go 1.17
+
+require github.com/go-sql-driver/mysql v1.6.0
---
+++ mysql.go
@@ -0,0 +1,205 @@
+package gomysql
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// db database object
+var db *sql.DB
+
+// ErrorTransactionNotOpened transaction not opened
+var ErrorTransactionNotOpened = errors.New("mysql: please open the transaction first")
+
+// Open connect to mysql service, auto set database connect; dsn: runner:112233@tcp(127.0.0.1:3306)/running?charset=utf8mb4&collation=utf8mb4_unicode_ci
+func Open(dsn string) (err error) {
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(512)
+	db.SetMaxIdleConns(128)
+	return
+}
+
+func Db0(database *sql.DB) {
+	db = database
+}
+
+func Db1() *sql.DB {
+	return db
+}
+
+func Exec() *Execs {
+	return &Execs{
+		db: db,
+	}
+}
+
+func Query(anonymous func(rows *sql.Rows) (err error), prepare string, args ...interface{}) error {
+	return Exec().OneStepQuery(anonymous, prepare, args...)
+}
+
+func Execute(prepare string, args ...interface{}) (int64, error) {
+	return Exec().OneStepExecute(prepare, args...)
+}
+
+func AddOne(prepare string, args ...interface{}) (int64, error) {
+	return Exec().OneStepAddOne(prepare, args...)
+}
+
+func Transaction(times int, anonymous func(execs *Execs) (err error)) error {
+	return Exec().Transaction(times, anonymous)
+}
+
+// Execs mysql database sql statement execute object
+type Execs struct {
+	db      *sql.DB                          // database connection object
+	tx      *sql.Tx                          // database transaction object
+	prepare string                           // sql statement to be executed
+	args    []interface{}                    // executed sql parameters
+	scan    func(rows *sql.Rows) (err error) // scan query results
+}
+
+func (s *Execs) Begin() (err error) {
+	s.tx, err = s.db.Begin()
+	return
+}
+
+func (s *Execs) Rollback() (err error) {
+	if s.tx == nil {
+		err = ErrorTransactionNotOpened
+		return
+	}
+	err = s.tx.Rollback()
+	s.tx = nil
+	return
+}
+
+func (s *Execs) Commit() (err error) {
+	if s.tx == nil {
+		err = ErrorTransactionNotOpened
+		return
+	}
+	err = s.tx.Commit()
+	s.tx = nil
+	return
+}
+
+func (s *Execs) Scan(anonymous func(rows *sql.Rows) (err error)) *Execs {
+	s.scan = anonymous
+	return s
+}
+
+func (s *Execs) Prepare(prepare string) *Execs {
+	s.prepare = prepare
+	return s
+}
+
+func (s *Execs) Args(args ...interface{}) *Execs {
+	s.args = args
+	return s
+}
+
+func (s *Execs) Stmt() (stmt *sql.Stmt, err error) {
+	if s.tx != nil {
+		stmt, err = s.tx.Prepare(s.prepare)
+	} else {
+		stmt, err = s.db.Prepare(s.prepare)
+	}
+	return
+}
+
+func (s *Execs) FetchSql() (prepare string, args []interface{}) {
+	prepare, args = s.prepare, s.args
+	return
+}
+
+func (s *Execs) Query() (err error) {
+	var stmt *sql.Stmt
+	stmt, err = s.Stmt()
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	var rows *sql.Rows
+	rows, err = stmt.Query(s.args...)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	err = s.scan(rows)
+	return
+}
+
+func (s *Execs) Execute() (rowsAffected int64, err error) {
+	var stmt *sql.Stmt
+	stmt, err = s.Stmt()
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	var result sql.Result
+	result, err = stmt.Exec(s.args...)
+	if err != nil {
+		return
+	}
+	rowsAffected, err = result.RowsAffected()
+	return
+}
+
+func (s *Execs) AddOne() (lastId int64, err error) {
+	var stmt *sql.Stmt
+	stmt, err = s.Stmt()
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	var result sql.Result
+	result, err = stmt.Exec(s.args...)
+	if err != nil {
+		return
+	}
+	lastId, err = result.LastInsertId()
+	return
+}
+
+func (s *Execs) OneStepQuery(anonymous func(rows *sql.Rows) (err error), prepare string, args ...interface{}) (err error) {
+	err = s.Scan(anonymous).Prepare(prepare).Args(args...).Query()
+	return
+}
+
+func (s *Execs) OneStepExecute(prepare string, args ...interface{}) (int64, error) {
+	return s.Prepare(prepare).Args(args...).Execute()
+}
+
+func (s *Execs) OneStepAddOne(prepare string, args ...interface{}) (int64, error) {
+	return s.Prepare(prepare).Args(args...).AddOne()
+}
+
+// Transaction closure execute transaction, automatic rollback on error
+func (s *Execs) Transaction(times int, anonymous func(execs *Execs) (err error)) (err error) {
+	if times <= 0 {
+		err = fmt.Errorf("mysql: the number of transactions executed by the database has been used up")
+		return
+	}
+	for i := 0; i < times; i++ {
+		err = s.Begin()
+		if err != nil {
+			continue
+		}
+		err = anonymous(s)
+		if err != nil {
+			_ = s.Rollback()
+			continue
+		}
+		_ = s.Commit()
+		break
+	}
+	return
+}
