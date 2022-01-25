d7572a341e1257da29b97dd6ccd6ecb105568227
cosasce <cosasce@gmail.com>
2022-01-25 10:07:28 +0800

    v1.1

--- helper.go
+++ helper.go
@@ -1,24 +1,365 @@
 package gomysql
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"reflect"
 	"sort"
+	"strings"
 )
 
-func Modify(modify map[string]interface{}) (columns []string, args []interface{}) {
-	length := len(modify)
+// modify 根据需要更新的map数据转化成字段切片和字段参数
+func modify(update map[string]interface{}) (columns []string, args []interface{}) {
+	length := len(update)
 	if length == 0 {
 		return
 	}
-	columns = make([]string, length, length)
-	args = make([]interface{}, length, length*2)
+	columns = make([]string, length)
+	args = make([]interface{}, length)
 	i := 0
-	for key := range modify {
+	for key := range update {
 		columns[i] = key
 		i++
 	}
 	sort.Strings(columns)
 	for key, val := range columns {
-		args[key] = modify[val]
+		args[key] = update[val]
 	}
+	return
+}
+
+// ModifyPrepareArgs 根据更新的map转化成sql更新脚本和对应参数
+func ModifyPrepareArgs(update map[string]interface{}) (prepare string, args []interface{}) {
+	var columns []string
+	columns, args = modify(update)
+	for key, val := range columns {
+		columns[key] = fmt.Sprintf("%s = ?", Identifier(val))
+	}
+	prepare = strings.Join(columns, ", ")
+	return
+}
+
+// Curd insert, update, delete, select
+type Curd struct {
+	hat *Hat
+}
+
+func NewCurd(hat ...*Hat) (curd *Curd) {
+	curd = &Curd{}
+	length := len(hat)
+	if length > 0 {
+		curd.hat = hat[length-1]
+	} else {
+		curd.hat = Db2()
+	}
+	return
+}
+
+// Name0 mysql name to go name
+func (s *Curd) Name0(name0 func(name string) string) {
+	s.hat.name0 = name0
+}
+
+// Name1 go name to mysql name
+func (s *Curd) Name1(name1 func(name string) string) {
+	s.hat.name1 = name1
+}
+
+// Transaction 必包执行事务, err != nil 自动回滚
+func (s *Curd) Transaction(fc func(hat *Hat) (err error)) error {
+	return s.hat.Transaction(fc)
+}
+
+// Begin 开起事务
+func (s *Curd) Begin() error {
+	return s.hat.Begin()
+}
+
+// Rollback 事务回滚
+func (s *Curd) Rollback() error {
+	return s.hat.Rollback()
+}
+
+// Commit 事务提交
+func (s *Curd) Commit() error {
+	return s.hat.Commit()
+}
+
+// Select 执行任何查询sql, 根据命名规则自动匹配
+func (s *Curd) Select(any interface{}, prepare string, args ...interface{}) error {
+	return s.hat.Prepare(prepare).Args(args...).Fetch(any)
+}
+
+// Fetch8First 查询第一条
+func (s *Curd) Fetch8First(prepare string, args ...interface{}) (map[string]*string, error) {
+	return s.hat.Prepare(prepare).Args(args...).Fetch8First()
+}
+
+// Fetch8All 查询所有
+func (s *Curd) Fetch8All(prepare string, args ...interface{}) ([]map[string]*string, error) {
+	return s.hat.Prepare(prepare).Args(args...).Fetch8All()
+}
+
+// Query 执行任何查询sql
+func (s *Curd) Query(anonymous func(rows *sql.Rows) (err error), prepare string, args ...interface{}) error {
+	return s.hat.Scan(anonymous).Prepare(prepare).Args(args...).Query()
+}
+
+// Execute 执行任何非查询sql
+func (s *Curd) Execute(prepare string, args ...interface{}) (int64, error) {
+	return s.hat.Prepare(prepare).Args(args...).Execute()
+}
+
+// Insert 执行插入一条的sql
+func (s *Curd) Insert(prepare string, args ...interface{}) (int64, error) {
+	return s.hat.Prepare(prepare).Args(args...).Create()
+}
+
+// isStructPointer 接口是否是一个结构体指针参数
+func (s *Curd) isStructPointer(any interface{}) bool {
+	if any == nil {
+		return false
+	}
+	tp := reflect.TypeOf(any)
+	if tp.Kind() != reflect.Ptr {
+		return false
+	}
+	return tp.Elem().Kind() == reflect.Struct
+}
+
+// InsertByMap 通过map[string]interface{}将数据插入数据库
+func (s *Curd) InsertByMap(insert map[string]interface{}, table string) (id int64, err error) {
+	if table == "" {
+		err = errors.New("the insert table is empty")
+		return
+	}
+	if insert == nil {
+		err = errors.New("the insert object is nil")
+		return
+	}
+	length := len(insert)
+	columns := make([]string, length)
+	values := make([]string, length)
+	args := make([]interface{}, length)
+	i := 0
+	for key := range insert {
+		columns[i] = key
+		values[i] = "?"
+		i++
+	}
+	// 根据字段名称排序
+	sort.Strings(columns)
+	for key, val := range columns {
+		args[key] = insert[val]
+	}
+	prepare := fmt.Sprintf(
+		"INSERT INTO %s ( %s ) VALUES ( %s );",
+		Identifier(table),
+		fmt.Sprintf("%s%s%s", Backtick, strings.Join(columns, fmt.Sprintf("%s, %s", Backtick, Backtick)), Backtick),
+		strings.Join(values, ", "),
+	)
+	id, err = s.Insert(prepare, args...)
+	return
+}
+
+// InsertByStruct 通过结构体指针将数据插入数据库, 可以指定表名, 表名以最后一个字符串为准, 不设置默认结构体名称转换
+func (s *Curd) InsertByStruct(insert interface{}, table ...string) (id int64, err error) {
+	if !s.isStructPointer(insert) {
+		err = fmt.Errorf("the insert object is not a struct pointer")
+		return
+	}
+	add := map[string]interface{}{}
+	val := reflect.ValueOf(insert)
+	vs := val.Elem()
+	for i := 0; i < vs.NumField(); i++ {
+		add[s.hat.name1(vs.Type().Field(i).Name)] = vs.Field(i).Interface()
+	}
+	tab := ""
+	length := len(table)
+	if length > 0 {
+		tab = table[length-1]
+	} else {
+		tab = s.hat.name1(vs.Type().Name())
+	}
+	id, err = s.InsertByMap(add, tab)
+	return
+}
+
+// Create 通过结构体指针将数据插入数据库自动设置自增长字段(bigint<=>int64)id值, 可以指定表名, 表名以最后一个字符串为准, 不设置默认结构体名称转换
+func (s *Curd) Create(insert interface{}, table ...string) (err error) {
+	if !s.isStructPointer(insert) {
+		err = fmt.Errorf("the insert object is not a struct pointer")
+		return
+	}
+	add := map[string]interface{}{}
+	val := reflect.ValueOf(insert)
+	vs := val.Elem()
+	idi := -1
+	tmp := ""
+	for i := 0; i < vs.NumField(); i++ {
+		tmp = vs.Type().Field(i).Name
+		add[s.hat.name1(tmp)] = vs.Field(i).Interface()
+		if strings.EqualFold(tmp, "id") {
+			idi = i
+		}
+	}
+	tab := ""
+	length := len(table)
+	if length > 0 {
+		tab = table[length-1]
+	} else {
+		tab = s.hat.name1(vs.Type().Name())
+	}
+	var id int64
+	id, err = s.InsertByMap(add, tab)
+	if err != nil {
+		return
+	}
+	// 插入对象的结构体存在Id/ID字段, 插入行的自增长值是正整数, 结构体属性可以设置, 结构体属性类型为int64类型
+	if idi >= 0 && id > 0 && vs.Field(idi).CanSet() && vs.Field(idi).Type().Kind() == reflect.Int64 {
+		vs.Field(idi).SetInt(id)
+	}
+	return
+}
+
+// idEqual 创建 `id` = ? where 条件
+func (s *Curd) idEqual() string {
+	return fmt.Sprintf("%s = ?", Identifier("id"))
+}
+
+// Delete 删除表数据 使用where条件, where: DELETE FROM `table` WHERE ( where );
+func (s *Curd) Delete(table string, where string, args ...interface{}) (int64, error) {
+	if where == "" {
+		return s.Execute(fmt.Sprintf("DELETE FROM %s;", Identifier(table)))
+	}
+	return s.Execute(fmt.Sprintf("DELETE FROM %s WHERE ( %s );", Identifier(table), where), args...)
+}
+
+// DeleteById 根据id字段删除数据, 通常id字段是自增长字段亦是数据库表主键
+func (s *Curd) DeleteById(table string, id interface{}) (int64, error) {
+	return s.Delete(table, s.idEqual(), id)
+}
+
+// UpdateByMap 通过map[string]interface{}更新数据
+func (s *Curd) UpdateByMap(update map[string]interface{}, table string, where string, args ...interface{}) (int64, error) {
+	key, val := ModifyPrepareArgs(update)
+	prepare := ""
+	if where == "" {
+		prepare = fmt.Sprintf("UPDATE %s SET %s;", Identifier(table), key)
+	} else {
+		prepare = fmt.Sprintf("UPDATE %s SET %s WHERE ( %s );", Identifier(table), key, where)
+		val = append(val, args...)
+	}
+	return s.Execute(prepare, val...)
+}
+
+// UpdateByMapById 通过map[string]interface{}更新数据
+func (s *Curd) UpdateByMapById(modify map[string]interface{}, table string, id interface{}) (int64, error) {
+	return s.UpdateByMap(modify, table, s.idEqual(), id)
+}
+
+// Update 根据两个结构体数据更新表数据
+// before:源数据库的数据(结构体指针)
+// after:最新变化的数据(结构体指针)
+// 更新字段为 before和after有相同名称的字段且字段值不相等,把对应字段的值设置成after对应字段的值, 不更新的字段不应该存在于after结构体中; 如果after结构体的类型和before类型是一致的, 建议设置成相同的值以避免数据剧库数据更新
+func (s *Curd) Update(before interface{}, after interface{}, table string, where string, args ...interface{}) (rowsAffected int64, err error) {
+	if !s.isStructPointer(before) {
+		err = fmt.Errorf("the update object before is not a struct pointer")
+		return
+	}
+	if !s.isStructPointer(after) {
+		err = fmt.Errorf("the update object after is not a struct pointer")
+		return
+	}
+	beforeValue := reflect.ValueOf(before)
+	beforeValue1 := beforeValue.Elem()
+	beforeMap := map[string]interface{}{}
+	length := beforeValue1.NumField()
+	for i := 0; i < length; i++ {
+		beforeMap[beforeValue1.Type().Field(i).Name] = beforeValue1.Field(i).Interface()
+	}
+	afterValue := reflect.ValueOf(after)
+	afterValue1 := afterValue.Elem()
+	afterMap := map[string]interface{}{}
+	length = afterValue1.NumField()
+	for i := 0; i < length; i++ {
+		afterMap[afterValue1.Type().Field(i).Name] = afterValue1.Field(i).Interface()
+	}
+	mod := map[string]interface{}{}
+	for key, val := range afterMap {
+		beforeVal, ok := beforeMap[key]
+		if !ok {
+			continue
+		}
+		if reflect.DeepEqual(beforeVal, val) {
+			continue
+		}
+		mod[s.hat.name1(key)] = val
+	}
+	rowsAffected, err = s.UpdateByMap(mod, table, where, args...)
+	return
+}
+
+// UpdateById 根据id更新
+func (s *Curd) UpdateById(before interface{}, after interface{}, table string, id interface{}) (int64, error) {
+	return s.Update(before, after, table, s.idEqual(), id)
+}
+
+// Count sql统计表的数据条数
+func (s *Curd) Count(prepare string, args ...interface{}) (count int64, err error) {
+	err = s.Query(func(rows *sql.Rows) (err error) {
+		if rows.Next() {
+			err = rows.Scan(&count)
+		}
+		return
+	}, prepare, args...)
+	return
+}
+
+// SumInt sql int64 求和
+func (s *Curd) SumInt(prepare string, args ...interface{}) (sum int64, err error) {
+	err = s.Query(func(rows *sql.Rows) (err error) {
+		if rows.Next() {
+			var tmp *int64
+			err = rows.Scan(&tmp)
+			if err != nil {
+				return
+			}
+			if tmp != nil {
+				sum = *tmp
+			}
+		}
+		return
+	}, prepare, args...)
+	return
+}
+
+// SumFloat sql float64 求和
+func (s *Curd) SumFloat(prepare string, args ...interface{}) (sum float64, err error) {
+	err = s.Query(func(rows *sql.Rows) (err error) {
+		if rows.Next() {
+			var tmp *float64
+			err = rows.Scan(&tmp)
+			if err != nil {
+				return
+			}
+			if tmp != nil {
+				sum = *tmp
+			}
+		}
+		return
+	}, prepare, args...)
+	return
+}
+
+// Exists sql查询数据是存在
+func (s *Curd) Exists(prepare string, args ...interface{}) (exists bool, err error) {
+	err = s.Query(func(rows *sql.Rows) (err error) {
+		if rows.Next() {
+			exists = true
+		}
+		return
+	}, prepare, args...)
 	return
 }
--- mysql.go
+++ mysql.go
@@ -3,22 +3,22 @@ package gomysql
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
-	Backtick = "`"
+	Backtick = "`" // backtick
 )
 
-// db database object
+// db database connect object
 var db *sql.DB
 
-// ErrTransactionNotOpened transaction not opened
-var ErrTransactionNotOpened = errors.New("go-mysql: please open the transaction first")
-
-// Open connect to mysql service, auto set database connect; dsn: runner:112233@tcp(127.0.0.1:3306)/running?charset=utf8mb4&collation=utf8mb4_unicode_ci
+// Open connect to mysql service, auto set database connect; dsn: yinran:112233@tcp(127.0.0.1:3306)/test?charset=utf8mb4&collation=utf8mb4_unicode_ci
 func Open(dsn string) (err error) {
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
@@ -30,91 +30,179 @@ func Open(dsn string) (err error) {
 	return
 }
 
+// Db0 set database connect object
 func Db0(database *sql.DB) {
 	db = database
 }
 
+// Db1 get database connect object
 func Db1() *sql.DB {
 	return db
 }
 
+// defaultName0 mysql name to go name
+var defaultName0 = func(name string) string {
+	return UnderlineToPascal(strings.ToLower(name))
+}
+
+// defaultName1 go name to mysql name
+var defaultName1 = func(name string) string {
+	return PascalToUnderline(name)
+}
+
+// Db2 database curd object
 func Db2() *Hat {
 	return &Hat{
-		db: db,
+		db:    db,
+		name0: defaultName0,
+		name1: defaultName1,
 	}
 }
 
+// Identifier MySql标识符
+func Identifier(s string) string {
+	if strings.Contains(s, "(") {
+		// 存在函数调用的标识符, 不做任何处理
+		return s
+	}
+	s = strings.ReplaceAll(s, Backtick, "")
+	s = strings.ReplaceAll(s, ".", fmt.Sprintf("%s.%s", Backtick, Backtick))
+	s = fmt.Sprintf("%s%s%s", Backtick, s, Backtick)
+	return s
+}
+
+// PascalToUnderline XxxYyy to xxx_yyy
+func PascalToUnderline(s string) string {
+	var tmp []byte
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			tmp = append(tmp, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		tmp = append(tmp, d)
+	}
+	return strings.ToLower(string(tmp[:]))
+}
+
+// UnderlineToPascal xxx_yyy to XxxYyy
+func UnderlineToPascal(s string) string {
+	var tmp []byte
+	bytes := []byte(s)
+	length := len(bytes)
+	nextLetterNeedToUpper := true
+	for i := 0; i < length; i++ {
+		if bytes[i] == '_' {
+			nextLetterNeedToUpper = true
+			continue
+		}
+		if nextLetterNeedToUpper && bytes[i] >= 'a' && bytes[i] <= 'z' {
+			tmp = append(tmp, bytes[i]-32)
+		} else {
+			tmp = append(tmp, bytes[i])
+		}
+		nextLetterNeedToUpper = false
+	}
+	return string(tmp[:])
+}
+
+// SetDefaultName0 mysql name to go name
+func SetDefaultName0(name0 func(name string) string) {
+	defaultName0 = name0
+}
+
+// SetDefaultName1 go name to mysql name
+func SetDefaultName1(name1 func(name string) string) {
+	defaultName1 = name1
+}
+
+// Query 执行查询sql
 func Query(anonymous func(rows *sql.Rows) (err error), prepare string, args ...interface{}) error {
 	return Db2().Scan(anonymous).Prepare(prepare).Args(args...).Query()
 }
 
+// Execute 执行非查询sql
 func Execute(prepare string, args ...interface{}) (int64, error) {
 	return Db2().Prepare(prepare).Args(args...).Execute()
 }
 
+// Transaction 事务执行,出错自动回滚
 func Transaction(anonymous func(hat *Hat) (err error)) error {
 	return Db2().Transaction(anonymous)
 }
 
+// Create 执行插入sql
 func Create(prepare string, args ...interface{}) (int64, error) {
 	return Db2().Prepare(prepare).Args(args...).Create()
 }
 
+// Fetch 查询sql, 根据命名规则自动匹配字段
 func Fetch(any interface{}, prepare string, args ...interface{}) (err error) {
 	return Db2().Prepare(prepare).Args(args...).Fetch(any)
 }
 
 // Hat mysql database sql statement execute object
 type Hat struct {
-	db               *sql.DB                          // database connection object
-	tx               *sql.Tx                          // database transaction object
-	prepare          string                           // sql statement to be executed
-	args             []interface{}                    // executed sql parameters
-	scan             func(rows *sql.Rows) (err error) // scan query results
-	column2attribute func(name string) string         // when driver scan column, table column name to struct attribute name.
-	attribute2column func(name string) string         // when driver insert table, struct attribute name to table column name.
+	db      *sql.DB                          // database connection object
+	tx      *sql.Tx                          // database transaction object
+	prepare string                           // sql statement to be executed
+	args    []interface{}                    // executed sql parameters
+	scan    func(rows *sql.Rows) (err error) // scan query results
+	name0   func(name string) string         // mysql name to go name
+	name1   func(name string) string         // go name to mysql name
 }
 
+// Begin 开启事务
 func (s *Hat) Begin() (err error) {
+	if s.tx != nil {
+		err = errors.New("please commit or rollback the opened transaction")
+		return
+	}
 	s.tx, err = s.db.Begin()
 	return
 }
 
+// Rollback 回滚事务
 func (s *Hat) Rollback() (err error) {
-	if s.tx == nil {
-		err = ErrTransactionNotOpened
-		return
+	if s.tx != nil {
+		err = s.tx.Rollback()
+		s.tx = nil
 	}
-	err = s.tx.Rollback()
-	s.tx = nil
 	return
 }
 
+// Commit 提交事务
 func (s *Hat) Commit() (err error) {
-	if s.tx == nil {
-		err = ErrTransactionNotOpened
-		return
+	if s.tx != nil {
+		err = s.tx.Commit()
+		s.tx = nil
 	}
-	err = s.tx.Commit()
-	s.tx = nil
 	return
 }
 
+// Scan 设置扫描查询结果(匿名函数)
 func (s *Hat) Scan(anonymous func(rows *sql.Rows) (err error)) *Hat {
 	s.scan = anonymous
 	return s
 }
 
+// Prepare 设置预处理sql语句
 func (s *Hat) Prepare(prepare string) *Hat {
 	s.prepare = prepare
 	return s
 }
 
+// Args 设置预处理sql语句的参数列表
 func (s *Hat) Args(args ...interface{}) *Hat {
 	s.args = args
 	return s
 }
 
+// stmt 执行预处理sql语句, 如果开始已经事务, 优先使用事务执行预处理sql语句
 func (s *Hat) stmt() (stmt *sql.Stmt, err error) {
 	if s.tx != nil {
 		stmt, err = s.tx.Prepare(s.prepare)
@@ -124,11 +212,13 @@ func (s *Hat) stmt() (stmt *sql.Stmt, err error) {
 	return
 }
 
+// PrepareArgs 获取预处理sql语句和预处理sql语句的参数列表
 func (s *Hat) PrepareArgs() (prepare string, args []interface{}) {
 	prepare, args = s.prepare, s.args
 	return
 }
 
+// Query 执行查询sql
 func (s *Hat) Query() (err error) {
 	var stmt *sql.Stmt
 	stmt, err = s.stmt()
@@ -146,6 +236,7 @@ func (s *Hat) Query() (err error) {
 	return
 }
 
+// Execute 执行非查询sql
 func (s *Hat) Execute() (rowsAffected int64, err error) {
 	var stmt *sql.Stmt
 	stmt, err = s.stmt()
@@ -162,6 +253,7 @@ func (s *Hat) Execute() (rowsAffected int64, err error) {
 	return
 }
 
+// Create 执行插入sql语句, 并获取自增长主键值
 func (s *Hat) Create() (lastId int64, err error) {
 	var stmt *sql.Stmt
 	stmt, err = s.stmt()
@@ -193,16 +285,205 @@ func (s *Hat) Transaction(anonymous func(hat *Hat) (err error)) (err error) {
 	return
 }
 
-func (s *Hat) ColumnToAttribute(column2attribute func(name string) string) {
-	s.column2attribute = column2attribute
+// Name0 mysql name to go name
+func (s *Hat) Name0(name0 func(name string) string) {
+	s.name0 = name0
 }
 
-func (s *Hat) AttributeToColumn(attribute2column func(name string) string) {
-	s.attribute2column = attribute2column
+// Name1 go name to mysql name
+func (s *Hat) Name1(name1 func(name string) string) {
+	s.name1 = name1
+}
+
+// scanning scan one or more rows.
+func (s *Hat) scanning(any interface{}, rows *sql.Rows, change func(name string) string) (err error) {
+	tp1 := reflect.TypeOf(any)
+	if tp1.Kind() != reflect.Ptr {
+		err = errors.New("sql: receive variable is not a pointer")
+		return
+	}
+	tp2 := tp1.Elem()
+	switch tp2.Kind() {
+	case reflect.Struct:
+		// any: *AnyStruct
+		if !rows.Next() {
+			// 查询结果集为空(没有查询到匹配的行)
+			return
+		}
+		var columns []string
+		columns, err = rows.Columns()
+		if err != nil {
+			return
+		}
+		var index int
+		var column string
+		if change == nil {
+			change = defaultName0
+		}
+		for index, column = range columns {
+			columns[index] = change(column)
+		}
+		var field reflect.Value
+		line := reflect.Indirect(reflect.New(tp2))
+		length := len(columns)
+		scanner := make([]interface{}, length)
+		cols := map[string]int{}
+		for i := 0; i < line.NumField(); i++ {
+			cols[line.Type().Field(i).Name] = i
+		}
+		var serial int
+		var ok bool
+		for index, column = range columns {
+			serial, ok = cols[column]
+			if !ok {
+				err = fmt.Errorf("struct field `%s` does not match", column)
+				return
+			}
+			field = line.Field(serial)
+			if !field.CanSet() {
+				err = fmt.Errorf("struct field `%s` cannot set value", column)
+				return
+			}
+			scanner[index] = field.Addr().Interface()
+		}
+		err = rows.Scan(scanner...)
+		if err != nil {
+			return
+		}
+		reflect.ValueOf(any).Elem().Set(line)
+	case reflect.Slice:
+		tp3 := tp2.Elem()
+		switch tp3.Kind() {
+		// any: *[]*AnyStruct
+		case reflect.Ptr:
+			if tp3.Elem().Kind() == reflect.Struct {
+				var columns []string
+				columns, err = rows.Columns()
+				if err != nil {
+					return
+				}
+				var index int
+				var column string
+				if change == nil {
+					change = defaultName0
+				}
+				for index, column = range columns {
+					columns[index] = change(column)
+				}
+				var line reflect.Value
+				var value reflect.Value
+				var field reflect.Value
+				slices := reflect.ValueOf(any).Elem()
+				length := len(columns)
+				scanner := make([]interface{}, length)
+				lines := reflect.Indirect(reflect.New(tp1.Elem().Elem().Elem()))
+				cols := map[string]int{}
+				for i := 0; i < lines.NumField(); i++ {
+					cols[lines.Type().Field(i).Name] = i
+				}
+				var serial int
+				var ok bool
+				for rows.Next() {
+					line = reflect.New(tp1.Elem().Elem().Elem())
+					value = reflect.Indirect(line)
+					for index, column = range columns {
+						serial, ok = cols[column]
+						if !ok {
+							err = fmt.Errorf("struct field `%s` does not match", column)
+							return
+						}
+						field = value.Field(serial)
+						if !field.CanSet() {
+							err = fmt.Errorf("struct field `%s` cannot set value", column)
+							return
+						}
+						scanner[index] = field.Addr().Interface()
+					}
+					err = rows.Scan(scanner...)
+					if err != nil {
+						return
+					}
+					slices = reflect.Append(slices, line)
+				}
+				if slices.Len() == 0 {
+					// 查询结果集为空(没有查询到匹配的行)
+					reflect.ValueOf(any).Elem().Set(reflect.MakeSlice(tp2, 0, 0))
+					return
+				}
+				// 查询到结果, 通过反射设置查询结果值
+				reflect.ValueOf(any).Elem().Set(slices)
+			}
+		// any: *[]AnyStruct
+		case reflect.Struct:
+			var columns []string
+			columns, err = rows.Columns()
+			if err != nil {
+				return
+			}
+			var index int
+			var column string
+			if change == nil {
+				change = defaultName0
+			}
+			for index, column = range columns {
+				columns[index] = change(column)
+			}
+			var line reflect.Value
+			var value reflect.Value
+			var field reflect.Value
+			slices := reflect.ValueOf(any).Elem()
+			length := len(columns)
+			scanner := make([]interface{}, length)
+			lines := reflect.Indirect(reflect.New(tp1.Elem().Elem()))
+			cols := map[string]int{}
+			for i := 0; i < lines.NumField(); i++ {
+				cols[lines.Type().Field(i).Name] = i
+			}
+			var serial int
+			var ok bool
+			for rows.Next() {
+				line = reflect.New(tp1.Elem().Elem())
+				value = reflect.Indirect(line)
+				for index, column = range columns {
+					serial, ok = cols[column]
+					if !ok {
+						err = fmt.Errorf("struct field `%s` does not match", column)
+						return
+					}
+					field = value.Field(serial)
+					if !field.CanSet() {
+						err = fmt.Errorf("struct field `%s` cannot set value", column)
+						return
+					}
+					scanner[index] = field.Addr().Interface()
+				}
+				err = rows.Scan(scanner...)
+				if err != nil {
+					return
+				}
+				slices = reflect.Append(slices, line.Elem())
+			}
+			if slices.Len() == 0 {
+				// 查询结果集为空(没有查询到匹配的行)
+				reflect.ValueOf(any).Elem().Set(reflect.MakeSlice(tp2, 0, 0))
+				return
+			}
+			// 查询到结果, 通过反射设置查询结果值
+			reflect.ValueOf(any).Elem().Set(slices)
+		default:
+		}
+	default:
+		err = fmt.Errorf("sql: unsupported receive variable type *%s", tp2.Name())
+	}
+	return
 }
 
 // Fetch scan one or more rows to interface{}
 func (s *Hat) Fetch(any interface{}) (err error) {
+	if any == nil {
+		err = errors.New("receive object value is nil")
+		return
+	}
 	var stmt *sql.Stmt
 	stmt, err = s.stmt()
 	if err != nil {
@@ -215,9 +496,121 @@ func (s *Hat) Fetch(any interface{}) (err error) {
 		return
 	}
 	defer rows.Close()
-	err = Scanning(any, rows, s.column2attribute)
+	err = s.scanning(any, rows, s.name0)
 	if err != nil {
 		return
+	}
+	return
+}
+
+// Fetch8First scan one to map[string]*string 查询结果为空返回 => nil, nil
+func (s *Hat) Fetch8First() (first map[string]*string, err error) {
+	var stmt *sql.Stmt
+	stmt, err = s.stmt()
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	var rows *sql.Rows
+	rows, err = stmt.Query(s.args...)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	first, err = s.fetch8First(rows)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// Fetch8All scan all to []map[string]*string 查询结果为空返回 => []map[string]*string{}, nil
+func (s *Hat) Fetch8All() (all []map[string]*string, err error) {
+	var stmt *sql.Stmt
+	stmt, err = s.stmt()
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	var rows *sql.Rows
+	rows, err = stmt.Query(s.args...)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	all, err = s.fetch8All(rows)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// fetch8First 查询结果为空返回 => nil, nil
+func (s *Hat) fetch8First(rows *sql.Rows) (first map[string]*string, err error) {
+	if !rows.Next() {
+		return
+	}
+	var length int
+	var columns []string
+	var scanner []interface{}
+	columns, err = rows.Columns()
+	if err != nil {
+		return
+	}
+	length = len(columns)
+	first = map[string]*string{}
+	tmp := make([][]byte, length)
+	scanner = make([]interface{}, length)
+	for i := range tmp {
+		scanner[i] = &tmp[i]
+	}
+	err = rows.Scan(scanner...)
+	if err != nil {
+		return
+	}
+	for key, val := range tmp {
+		if val == nil {
+			first[columns[key]] = nil
+		} else {
+			str := string(val)
+			first[columns[key]] = &str
+		}
+	}
+	return
+}
+
+// fetch8All 查询结果为空返回 => []map[string]*string{}, nil
+func (s *Hat) fetch8All(rows *sql.Rows) (all []map[string]*string, err error) {
+	var length int
+	var columns []string
+	var scanner []interface{}
+	var line map[string]*string
+	columns, err = rows.Columns()
+	if err != nil {
+		return
+	}
+	length = len(columns)
+	all = []map[string]*string{}
+	for rows.Next() {
+		tmp := make([][]byte, length)
+		scanner = make([]interface{}, length)
+		for i := range tmp {
+			scanner[i] = &tmp[i]
+		}
+		err = rows.Scan(scanner...)
+		if err != nil {
+			return
+		}
+		line = map[string]*string{}
+		for key, val := range tmp {
+			if val == nil {
+				line[columns[key]] = nil
+			} else {
+				str := string(val)
+				line[columns[key]] = &str
+			}
+		}
+		all = append(all, line)
 	}
 	return
 }
